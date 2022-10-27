95879ad4ddd968dfb1154b34e6d17db09eace991
Luciano Marchueta <lucianomarchueta@gmail.com>
2022-10-27 17:28:33 -0300

    cambios en desafio

---
+++ productos.js
@@ -0,0 +1,25 @@
+[
+    {
+        "title": "pelota",
+        "price": 50,
+        "id": 0
+    },
+
+    {
+        "title": "botines",
+        "price": 150,
+        "id": 1
+    },
+
+    {
+        "title": "camiseta",
+        "price": 100,
+        "id": 2
+    },
+
+    {
+        "title": "short",
+        "price": 80,
+        "id": 3
+    }
+]
